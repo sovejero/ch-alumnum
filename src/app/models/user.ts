6b009c5be4d051dfe8bf426e85c7fc4496b3b9a3
Santiago Ovejero <santiagodovejero@gmail.com>
2022-12-21 10:53:10 -0300

    refactor: replace class to interface models

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,12 +1,9 @@
-export class User {
-  
-  constructor(
-    public id: number,
-    public email: string,
-    public password: string,
-    public name: string,
-    public address: string,
-    public phone: string,
-    public profile: string
-  ) {}
+export interface User {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  address: string;
+  phone: string;
+  profile: string;
 }
