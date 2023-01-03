b2d34e0ecc7d42081b6925ee4775ee88840b5ded
Santiago Ovejero <santiagodovejero@gmail.com>
2023-01-03 14:34:45 -0300

    feat: add sidenav open and close functionality

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  opened: boolean = true;
+
+  toggleSidenav(): void {
+    this.opened = !this.opened
+  }
 
   constructor() { }
 
